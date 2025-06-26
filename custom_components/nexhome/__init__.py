22894c7fd7f6324916b5a2d92df12403e08a7cfb
zgx327238349 <50612623+zgx327238349@users.noreply.github.com>
2025-06-26 09:11:58 +0800

    Update __init__.py

--- custom_components/nexhome/__init__.py
+++ custom_components/nexhome/__init__.py
@@ -8,18 +8,12 @@ from .utils import set_hass_obj
 # discoverObj = None
 
 async def async_setup_entry(hass, entry):
-    # global discoverObj
     await register_device_list_service(hass, entry)
-    for platform in ALL_PLATFORM:
-        await hass.async_create_task(hass.config_entries.async_forward_entry_setups(
-            entry, platform))
+    await hass.config_entries.async_forward_entry_setups(entry, ALL_PLATFORM)
+    return True
 
-    # discoverObj = hass.data[DOMAIN].get(DISCOVER)
-    # if discoverObj is None:
-    #     discoverObj = await discover(hass)
-    # else:
-    #     await discoverObj.start()
-
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    await hass.config_entries.async_forward_entry_unloads(entry, ALL_PLATFORM)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
